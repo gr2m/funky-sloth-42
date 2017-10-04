f758c3217eb7e83e9214dc421193552689a4ff1a
Gregor <gregor@martynus.net>
2017-10-04 01:28:44 -0700

    refactor: move randomAdjective() in separate file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = generateRandomString
 
-const adjectives = require('adjectives')
 const animals = require('animals')
 const randomNumber = require('random-number-in-range')
+
+const randomAdjective = require('./lib/random-adjective')
 
 function generateRandomString (num) {
   if (typeof num === 'number') {
@@ -10,8 +11,4 @@ function generateRandomString (num) {
   }
 
   return [randomAdjective(), animals(), randomNumber(10, 99)].join('-')
-}
-
-function randomAdjective () {
-  return adjectives[randomNumber(0, adjectives.length - 1)]
 }
